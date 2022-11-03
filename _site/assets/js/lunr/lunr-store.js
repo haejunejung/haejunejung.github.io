c4a0ae740690c7804a5af3d95cac8bea2f0486c8
haejunejung <jhj9422@unist.ac.kr>
2022-11-03 20:40:38 +0900

    2022

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,25 @@
-var store = []
+var store = [{
+        "title": "정해준의 개발 일지의 시작",
+        "excerpt":"깃허브 블로그를 쓰기 시작한 날 -2022.10.29- 왜 블로그를 하게 되었나? 개발자들이 다들 Today I learn… TIL… 이렇게 말하길래 나도 써볼까해서 써본다. 솔직히 얼마나 도움이 될 지는 모르겠다. 그래도 안하는 것보다 낫지않을까? 블로그를 하면서 내가 공부했던 것들을 정리하는 시간을 가질 수 있지 않을까? 라는 생각으로 쓰게 되었다 앞으로도 계속 풀게 될...","categories": ["블로그"],
+        "tags": [],
+        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8/start/",
+        "teaser": null
+      },{
+        "title": "Merge Sort",
+        "excerpt":"Merge Sort 알고리즘 정렬 알고리즘 중 시간 복잡도 O(nlongn)을 가지는 알고리즘 같은 시간 복잡도를 가지는 알고리즘으로는 Quick Sort가 있다 Quick Sort에 비해 공간 복잡도를 더 많이 사용하게 된다 이미 정렬되어 있는 배열에 대해서 정렬을 하는 특정 상황에서는 다른 정렬 알고리즘이 더 빠를 수 있다 Code 재귀를 사용하여 배열에 있는 모든...","categories": ["알고리즘"],
+        "tags": [],
+        "url": "/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98/mergesort/",
+        "teaser": null
+      },{
+        "title": "하노이 탑",
+        "excerpt":"하노이 탑 알고리즘 하노이 탑 재귀함수를 이해할 수 있는 좋은 문제 중 하나이다 재귀를 사용하기 위해서는 Base case와 Inductive case를 잘 나눠야 한다 하노이 탑의 경우, N개의 블럭을 옮길 때 N이 1인 경우가 Base case에 해당한다 하노이 탑의 기본적인 틀은 1, 2, 3으로 움직일 수 있다고 하고, 1에 있는 N개의...","categories": ["알고리즘"],
+        "tags": [],
+        "url": "/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98/hanoi/",
+        "teaser": null
+      },{
+        "title": "최대공약수 GCD와 최소공배수 LCM",
+        "excerpt":"최대공약수와 최소공배수를 효율적으로 구하는 알고리즘 GCD (Greatest Common Divisor) 최대공약수는 두 수의 공약수 중 가장 큰 공약수를 말한다 첫 번째 방법은 1부터 두 수 중 작은 수까지 두 수를 모두 나누어봤을 때, 두 나머지가 0이 되는 수 중 가장 큰 수를 찾는 것이다 첫 번째 방법은 1부터 n(작은 수)까지 모두...","categories": ["알고리즘"],
+        "tags": [],
+        "url": "/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98/GCD,LCD,CommonDivisor/",
+        "teaser": null
+      }]
