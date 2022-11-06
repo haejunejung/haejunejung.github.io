92b02a8e5bc094634f7fba187f38cbdf26a8226b
haejunejung <jhj9422@unist.ac.kr>
2022-11-06 14:12:59 +0900

    2022

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -22,4 +22,40 @@ var store = [{
         "tags": [],
         "url": "/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98/GCD,LCD,CommonDivisor/",
         "teaser": null
+      },{
+        "title": "스도쿠",
+        "excerpt":"스도쿠 백준 2580번 스도쿠 풀이 입력으로 들어오는 값들을 저장한다. 이 때, 빈칸으로 오는 곳은 따로 저장한다 빈칸을 하나씩 채워가며 모두 채웠다면 출력을 하고 종료한다 (Base Case) 이 때, return을 하는 것이 아니라 exit를 해줘야 한다. exit하지 않는다면, 다른 방법으로도 되는 경우 출력이 될 수 있기 때문이다 check 함수에서 빈 칸에...","categories": ["백준"],
+        "tags": [],
+        "url": "/%EB%B0%B1%EC%A4%80/sudoku/",
+        "teaser": null
+      },{
+        "title": "가장 긴 바이토닉 부분 수열",
+        "excerpt":"가장 긴 바이토닉 부분 수열 백준 11054번 가장 긴 바이토닉 부분 수열 풀이 처음에는 문제를 잘못 이해해서, 바이토닉 부분 수열이 끊기지 않고 연결되야 하는 것으로 생각했다. 문제의 밑에 힌트를 보고 꼭 연결되지 않아도 바이토닉 부분 수열을 만족하기만 하면 된다는 것을 알게 되었다. 풀이 방법은, dp를 모두 1로 초기화시킨다. (최소 길이가...","categories": ["백준"],
+        "tags": [],
+        "url": "/%EB%B0%B1%EC%A4%80/%EA%B0%80%EC%9E%A5-%EA%B8%B4-%EB%B0%94%EC%9D%B4%ED%86%A0%EB%8B%89-%EB%B6%80%EB%B6%84-%EC%88%98%EC%97%B4/",
+        "teaser": null
+      },{
+        "title": "Maximum vs Maximal",
+        "excerpt":"Maximum과 Maximal의 차이점 Maximum (Minimum) 우리가 고려하는 것 중에서 가장 많은 것을 의미한다 MaxiMal (Minimal) Maximal이라는 단어의 앞에는 ‘inclusion-wise’가 생략되어 있다 즉, 포함 관계에 따라 최대인 것을 의미한다 Property Maximum인 것은 Maximal이기도 하다 하지만 Maximal인 것은 Maximum하다고 할 수 없다 (동일하지 않을 때가 존재한다) 정의에 의해 Maximum을 이루는 것들의 크기...","categories": ["추가공부자료"],
+        "tags": [],
+        "url": "/%EC%B6%94%EA%B0%80%EA%B3%B5%EB%B6%80%EC%9E%90%EB%A3%8C/maximal&maximum/",
+        "teaser": null
+      },{
+        "title": "Ch13.induction",
+        "excerpt":"Induction Mathematical induction (수학적 귀납법) 수학적 귀납법 수학적 귀납법이란 수학의 증명 방법 중 하나로, 주로 어떠한 명제가 모든 자연수에 대하여 성립함을 보이려고 할 때, 이용된다 수학적 귀납법의 일반적인 형식은 5단계로 구분된다 수학적 귀납법을 사용할 것이라고 명시한다. 어떤 명제에 대하여 성립함을 보일 것인지 명시한다. Base case에 대하여 성립함을 보인다. Inductive case에...","categories": ["이산수학"],
+        "tags": [],
+        "url": "/%EC%9D%B4%EC%82%B0%EC%88%98%ED%95%99/induction/",
+        "teaser": null
+      },{
+        "title": "전깃줄",
+        "excerpt":"전깃줄 백준 2565번 전깃줄 풀이 처음에는 전깃줄의 정보를 모두 받고 연결하였을 때 교차되는 점의 개수를 구하여 가장 많이 교차되는 부분부터 삭제하는 방식을 사용하려고 하였다. 백준에 나와있는 예시 입력과 출력은 정답을 맞출 수 있었지만 결과를 틀렸다고 나왔다. 어떤 부분에서 틀렸는지 찾아보니, 교차되는 점의 개수가 같을 때 없애는 순서에 따라 결과값이 달라진다는...","categories": ["백준"],
+        "tags": [],
+        "url": "/%EB%B0%B1%EC%A4%80/%EC%A0%84%EA%B9%83%EC%A4%84/",
+        "teaser": null
+      },{
+        "title": "Ch14.Recursion",
+        "excerpt":"Recursion Recursion (재귀) Recursion 재귀(Recursion)는 새로운 Object를 만들기 위해서 사용된다 반면, 귀납법(Induction)은 증명을 하기 위해서 사용된다 함수 f (domain N) 을 정의하기 위해 2가지 단계로 구분된다 Basis step: Specify f(0) Recursive step: n보다 작은 자연수로 f(n)을 찾기 위한 규칙들 이것을 함수 f의 Recursive or Inductive definition라고 한다 Structural Induction Basis...","categories": ["이산수학"],
+        "tags": [],
+        "url": "/%EC%9D%B4%EC%82%B0%EC%88%98%ED%95%99/Recursion/",
+        "teaser": null
       }]
