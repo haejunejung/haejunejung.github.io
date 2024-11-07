d461e9ae11497b3c2689a562f5f8c8ffc4a453ad
haejunejung <jhj9422@unist.ac.kr>
2024-11-07 17:40:25 +0900

    [fix]: Workbox inactivate

--- vite.config.ts
+++ vite.config.ts
@@ -19,10 +19,10 @@ export default defineConfig({
       registerType: "autoUpdate",
       injectRegister: "inline",
       devOptions: { enabled: true },
-      workbox: {
-        globPatterns: ["/*"],
-        globIgnores: ["**/node_modules/**/*", "sw.js", "workbox-*.js"],
-      },
+      // workbox: {
+      //   globPatterns: ["/*"],
+      //   globIgnores: ["**/node_modules/**/*", "sw.js", "workbox-*.js"],
+      // },
     }),
     react({ include: /\.(jsx|js|mdx|md|tsx|ts)$/ }),
   ],
