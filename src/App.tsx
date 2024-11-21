72b0c92d18da746b4c5ebafcda5002e1fc21aa27
haejunejung <jhj9422@unist.ac.kr>
2024-11-21 17:13:21 +0900

    [feat]: Web, Android, IOS 브라우저 별 웹 폰트 불러오기

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,23 @@ import Header from "./components/Header";
 import MDXPage from "./pages/MDXPage";
 import TechBlogPage from "./pages/TechBlogPage";
 import { totalMdxPageList } from "./mdx";
+import { useEffect } from "react";
 
 const App = () => {
   // //TODO: Switch ~ Route 패턴이 CSR이라서 PWA에서 동작하지 않는 문제 해결 필요!
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+
+    if (/Android/i.test(userAgent)) {
+      document.body.classList.add("android");
+    } else if (/iPhone|iPad|iPod/i.test(userAgent)) {
+      document.body.classList.add("ios");
+    } else {
+      document.body.classList.add("web");
+    }
+  }, []);
+
   return (
     <Router>
       <Layout>
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import { createRoot } from "react-dom/client";
 import { MDXProvider } from "@mdx-js/react";
 import { MDXComponents } from "./components/[mdx]/index.ts";
 import App from "./App.tsx";
-import "@/styles/main.scss";
-import "highlight.js/styles/github.css";
 import { ThemeProvider } from "./contexts/theme/ThemeProvider.tsx";
+import "highlight.js/styles/github.css";
+import "./global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
