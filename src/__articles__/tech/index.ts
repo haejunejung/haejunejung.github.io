839abf1f892333109fdc65626383cca34f213f06
haejunejung <jhj9422@unist.ac.kr>
2025-02-09 18:35:39 +0900

    [feat]: 피그마 플러그인 개발 Part(1)

--- src/__articles__/tech/index.ts
+++ src/__articles__/tech/index.ts
@@ -3,10 +3,10 @@ import { ArticlePageProps } from "@/types";
 export const techList: ArticlePageProps[] = [
   {
     src: "/images/fds-figma-plugin.png",
-    title: "띵동! 아이콘이 도착했어요.",
+    title: "띵동! 아이콘이 도착했어요 (1)",
     description: "아이콘을 쉽게 관리할 수 있는 피그마 플러그인을 개발했어요",
-    slug: "/tech/fds-figma-plugin",
-    date: "2024년 2월 9일",
-    importMdx: () => import("./fds-figma-plugin.mdx")
+    slug: "/tech/fds-figma-plugin-1",
+    date: "2024년 2월 2일",
+    importMdx: () => import("./fds-figma-plugin-1.mdx")
   }
 ];
